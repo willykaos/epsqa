9ad2234a9600f77b0c0707782efd444ffc5e94cf
Willy <willykaos@gmail.com>
2025-04-21 19:44:48 -0500

    Base

---
+++ src/main/java/edu/ucaldas/epsqa/EpsqaApplication.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.epsqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsqaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ucaldas/epsqa/EpsqaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.epsqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
